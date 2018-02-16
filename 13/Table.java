0157206e893740c96553d43688bc4ddfca3f0764
ALExWang6 <31966312+ALExWang6@users.noreply.github.com>
2018-02-16 14:20:55 -0500

    Add files via upload

---
+++ 13/Table.java
@@ -0,0 +1,26 @@
+public class Table {
+
+    public static void main (String[] args) {
+    
+        String sub1 = "Philosophy";
+        String sub2 = "Math";
+        String sub3 = "Com Science";
+        String sub4 = "Physics";
+        
+        String name1 = "Mr Parravani";
+        String name2 = "Ms Bocci";
+        String name3 = "Mr Gallo";
+        String name4 = "Mr leduc";
+        
+        System.out.println("+------------------------------------------+");
+        System.out.println("| 1 |           " + name1 + " |  " + sub1 + " |       ");
+        System.out.println("| 2 |               " + name2 + " |        " + sub2 + " |       ");
+        System.out.println("| 3 |               " + name3 + " | " + sub3 + " |       ");
+        System.out.println("| 4 |               " + name4 + " |     " + sub4 + " |       ");
+        System.out.println("+------------------------------------------+");
+        
+    }
+    
+}   
+        
+        
