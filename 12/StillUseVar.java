6d1de9cc68043fc3d158e21f55bceb1ca7e4166d
ALExWang6 <31966312+ALExWang6@users.noreply.github.com>
2018-02-16 14:03:58 -0500

    Add files via upload

---
+++ 12/StillUseVar.java
@@ -0,0 +1,12 @@
+public class StillUseVar {
+
+    public static void main (String[] args) {
+    
+        int gradYear = 2018;
+        String name = "Alex";
+        
+        System.out.println("My name is " + name + " and I graduate in " + gradYear);
+        
+    }
+    
+}
